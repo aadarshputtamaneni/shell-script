6091a998f567b2aa3f650a2d2cdcd78d1ed55392
aadarsh <aadarsh153@gmail.com>
2025-04-24 21:07:53 +0530

    update

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,9 +3,19 @@
 #declaring of varible
 PERSON1=Sachin #no space between = and value
 PERSON2=Rahul
-
+#Two ways to access Variables, $varname i.e, $NAME01 or ${NAME01}.......
 #referring variable
 echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine. How are you?"
 echo "$PERSON1:: I am fine too. how is your work?"
 echo "$PERSON2:: not bad. I am thinking to upgrade to DevOps"
+
+
+PERSON1=virat
+PERSON2=dhoni
+
+echo "${PERSON1}:: Hello ${PERSON2}, How are you?"
+echo "${PERSON2}:: Hi ${PERSON1}, I am fine. How are you?"
+echo "${PERSON1}:: I am fine too. how is your work?"
+echo "${PERSON2}:: not bad. I am thinking to upgrade to DevOps"
+
