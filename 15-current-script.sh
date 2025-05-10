0a57bce5511aefa03110ebbe4657a835450eec75
aadarsh <aadarsh153@gmail.com>
2025-05-10 23:48:09 +0530

    update

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -10,3 +10,9 @@ echo "Process ID of current shell script: $$"
 source ./16-other-script.sh
 
 echo "After calling other script, course: $COURSE"
+
+#1st way  ./<script-name>.sh
+#changes happened in other script will not affect current script,PID of 2 scripts are different.
+#2nd way  source ./<script-name>.sh
+#changes happened in other script will be reflected in current script,PID of2 scripts are same.
+
